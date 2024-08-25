25a248d450b9753c7bd7ed3c1277b27299f0f1da
Gordon Hulpieu <ghulpieu@gmail.com>
2024-08-25 12:48:53 -0400

    refactor: convert nodejs app to run in a container

--- assignment-mta/index.js
+++ assignment-mta/index.js
@@ -1,3 +1,6 @@
+// Commandline for Windows:
+//   docker container run -v ${PWD}/in:/app/in -v ${PWD}/out:/app/out --env CHARCOAL_FACTOR=10 node-mta
+
 // A legacy app that needs migrated to docker
 // Requires node 8.x and GraphicsMagick installed on OS
 // It will pull images from a hardcoded dir and output
@@ -12,18 +15,20 @@ var fs      = require('fs'),
 var inDir          = './in',
     outDir         = './out',
     logDir         = './logs',
-    charcoalFactor = process.env.CHARCOAL_FACTOR; // ideally 0.1 to 1.0
+    charcoalFactor = process.env.CHARCOAL_FACTOR || 0.1; // ideally 0.1 to 1.0
 
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.simple(),
   transports: [
+    // output to stdout stderr for Docker
+    new winston.transports.Console()
     //
-    // - Write to all logs with level `info` and below to `combined.log` 
+    // - Write to all logs with level `info` and below to `combined.log`
     // - Write all logs error (and below) to `error.log`.
     //
-    new winston.transports.File({ filename: logDir + '/error.log', level: 'error' }),
-    new winston.transports.File({ filename: logDir + '/combined.log' })
+    // new winston.transports.File({ filename: logDir + '/error.log', level: 'error' }),
+    // new winston.transports.File({ filename: logDir + '/combined.log' })
   ]
 });
 
